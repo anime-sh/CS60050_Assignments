55a61e813c68de839840ac2550e62847acb834da
JhadotAnimesh <jha.animesh01@gmail.com>
2021-10-17 20:13:07 +0530

    everything works

--- Assgn2/main.py
+++ Assgn2/main.py
@@ -1,109 +1,79 @@
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 from sklearn import preprocessing
 from tqdm import tqdm
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from scipy import sparse as sp
 import numpy as np
-df = pd.read_csv("train.csv")
-# IMPLEMENT THIS KHUDSE
-vec = CountVectorizer(stop_words='english', binary=True)
-M = vec.fit_transform(df['text'].to_numpy())
-M = M.toarray()
-le = preprocessing.LabelEncoder()
-le.fit(df.author)
-print(le.classes_)
-df.author = le.transform(df.author)
-print(M.shape)
-print(df.author.shape)
+import re
+from nltk.corpus import stopwords
+from model import NaiveBayes
 
-X_train, X_test, y_train, y_test = train_test_split(M, df.author.to_numpy(
-), stratify=df.author.to_numpy(), test_size=0.30, random_state=42)  # stratify ???
-print(X_train.shape)
-print(X_test.shape)
-print(y_train.shape)
-print(y_test.shape)
+def read_data():
+    df = pd.read_csv("train.csv")
+    return df
 
 
-class NaiveBayes(object):
-    def __init__(self, alpha=1, n_classes=3):
-        """
-        Initialize the Naive Bayes model with alpha (correction) and n_classes
-        """
-        self.alpha = alpha
-        self.n_classes = n_classes
-        self.X_train = None
-        self.Y_train = None
-        self.label_total_text_counts = {}
-        self.label_total_word_counts = {}
-        self.label_word_counts = {}
-        for i in range(n_classes):
-            self.label_total_text_counts[i] = 0.0
-            self.label_total_word_counts[i] = 0.0
-            self.label_word_counts[i] = []
+def get_M_matrix():
+    stops = set(stopwords.words('english'))
 
-    def fit(self, X_train, y_train):
-        """
-        Fit the model to X_train, y_train
-        """
-        # Count how many words per label, the frequency of the word for a label
-        self.Y_train = y_train
-        self.X_train = X_train
-        
-        for j in range(self.n_classes):
-            self.label_word_counts[j] = np.zeros(self.X_train.shape[1])
-        
-        for i in range(self.X_train.shape[0]):
-            self.label_total_text_counts[y_train[i]] += 1
-            self.label_word_counts[y_train[i]] += self.X_train[i]
-            self.label_total_word_counts[y_train[i]] += np.sum(self.X_train[i])
-        
+    vocab = {}
+    text_arr = df['text'].to_numpy()
+    for i in range(len(text_arr)):
+        for word in re.findall("[a-z0-9]+", text_arr[i].casefold()):
+            if (word not in stops) and (len(word) > 2):
+                vocab[word] = 1
 
-    def log_p_doc(self, x, y):
-        # # Calculate conditional probability P(word+alpha|label+vocab*alpha) (with smoothening)
-        # s=np.sum(self.label_word_counts[y]*x)+self.alpha  #can replace for loop with this
-        # s/=(self.label_total_word_counts[y]+self.X_train.shape[1]*self.alpha)
+    print(f"Number of distinct words in the dataset = {len(vocab)}")
+    M = np.zeros((len(text_arr), len(vocab)))
 
-        num=np.log(self.label_word_counts[y]+self.alpha) # broadcasting
-        denom=np.log(self.label_total_word_counts[y]+self.X_train.shape[0]*self.alpha)
-        num=num-denom # broadcasting
-        log_prob=np.sum(x*num)
-        return log_prob
+    idx = 0
+    for word in vocab:
+        vocab[word] = idx
+        idx += 1
 
-    def prior(self, y):
-        return self.label_total_text_counts[y]/self.X_train.shape[0]
+    for i in tqdm(range(len(text_arr))):
+        for word in re.findall("[a-z0-9]+", text_arr[i].casefold()):
+            if (word not in stops) and (len(word) > 2):
+                M[i][vocab[word]] = 1
+    return M
 
-    def predict(self, X_test):
-        """
-        Predict the X_test with the fitted model
-        """
-        pred = []
-        probs = []
-        priors = []
-        for i in range(self.n_classes):
-            priors.append(self.prior(i))
-        
-        for x in tqdm(X_test):
-            denom = 0
-            local_preds = []
-            for j in range(self.n_classes):
-                lolol = self.log_p_doc(x, j)
-                numerator = np.log(priors[j])+lolol
-                denom += priors[j]*np.exp(lolol)
-                local_preds.append(numerator)
-            denom = np.log(denom)
-            local_preds = np.array(local_preds)-denom  # broadcasting
-            pred.append(np.argmax(local_preds))
-            probs.append(np.exp(local_preds))
-            
-            
-        return pred, probs
+def train_test_split_data(M, y):
+    X_train, X_test, y_train, y_test = train_test_split(M, y, stratify=y, test_size=0.30, random_state=42) 
+    print(f" Shape X train {X_train.shape}")
+    print(f" Shape y train {y_train.shape}")
+    print(f" Shape X test {X_test.shape}")
+    print(f" Shape y test {y_test.shape}")
+    
+    return X_train, X_test, y_train, y_test
 
 
-nb=NaiveBayes()
-from sklearn.metrics import confusion_matrix, classification_report
 
-nb.fit(X_train,y_train)
-Y_pred,y_probs=nb.predict(X_test)
-print(confusion_matrix(y_test,Y_pred))
-print(classification_report(y_test,Y_pred))
+
+def run_experiment(X_train,X_test,y_train,y_test,le,alpha=1):
+    model = NaiveBayes(alpha=alpha,n_classes=len(le.classes_))
+    model.fit(X_train, y_train)
+    y_pred,y_probs = model.predict(X_test)
+    print(f"Full Classification report:")
+    print(classification_report(y_test, y_pred))
+    print(F"Confusion Matrix:")
+    print(confusion_matrix(y_test, y_pred))
+    print(F"Accuracy on test split = {accuracy_score(y_test,y_pred)}")
+
+
+if __name__ == "__main__":
+    df = read_data()
+    M = get_M_matrix()
+    le = preprocessing.LabelEncoder()
+    le.fit(df.author)
+    print(le.classes_)
+    df.author = le.transform(df.author)
+    print(M.shape)
+    print(df.author.shape)
+    X_train, X_test, y_train, y_test = train_test_split_data(M, df.author.to_numpy())
+    print(F"Experiment with no laplace correction:")
+    run_experiment(X_train,X_test,y_train,y_test,le,alpha=0)
+    print(F"Experiment with laplace correction, alpha=1:")
+    run_experiment(X_train,X_test,y_train,y_test,le,alpha=1)
+    print(F"Experiment with laplace correction, alpha=10:")
+    run_experiment(X_train,X_test,y_train,y_test,le,alpha=10)
+
